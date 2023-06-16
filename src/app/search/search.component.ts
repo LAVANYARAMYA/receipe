d6790072934b048192f6cc44b15c69cb8f9d4a1c
LAVANYARAMYA <lavanyaramya1947@gmail.com>
2023-06-16 12:45:15 +0530

    api call inital

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-// import { HttpClient } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 import { FormBuilder } from '@angular/forms';
 import { debounceTime, distinctUntilChanged, switchMap, timer } from 'rxjs';
@@ -10,28 +10,38 @@ import { debounceTime, distinctUntilChanged, switchMap, timer } from 'rxjs';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent {
-  // receipes:any;
+  
+   receipes:any;
 
-  receipes=[{
-    name:"tomato rice",
-    ingredients:'tomato*rice',
-    instructions:"Wash and drain the basmati rice and soak for about 20 minutes. Take a deep pan, put it on medium flame and pour water into it. Bring it to a boil, and then add the soaked rice. Cover the pan with a lid and cook on medium flame till the rice becomes soft, but not mushy",
-    servingsize:2,
-    preptime:30
-  },
-  {
-    name:"tomato curry",
-    ingredients:'tomato*rice*veg',
-    instructions:"Wash and drain the basmati rice and soak for about 20 minutes. Take a deep pan, put it on medium flame and pour water into it. Bring it to a boil, and then add the soaked rice. Cover the pan with a lid and cook on medium flame till the rice becomes soft, but not mushy",
-    servingsize:3,
-    preptime:20
-  }];
+  
 
-  constructor( private fb: FormBuilder) {
+  constructor( private http: HttpClient,private fb: FormBuilder) {
     
   }
   searchForm=this.fb.group({
     Ingredients: [''],
   });
+
+
+  ngOnInit()
+  {
+     this.searchForm
+     .get('Ingredients')
+     ?.valueChanges.pipe(
+      debounceTime(500),
+      distinctUntilChanged(),
+      switchMap((val)=>this.search(val as string)))
+      
+     
+     .subscribe((value:any)=>
+     {
+      this.receipes=value;
+     });
+}
+
+    search(receipe: string)
+     {
+        return this.http.get(`https://648a951417f1536d65e94e85.mockapi.io/receipes`);
+     }
 
 }
